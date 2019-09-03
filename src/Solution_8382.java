5d42430ebf6b973d93256d8c749d91596b686a4b
김경태 <kkt3526@naver.com>
2019-09-03 14:20:21 +0900

    2019-09-03

--- src/Solution_8382.java
+++ src/Solution_8382.java
@@ -1,20 +1,43 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution_8382 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		Scanner sc = new Scanner(System.in);
 		int iT = sc.nextInt();
 		
-=======
-		int[] citations = {3,0,6,1,5};
-		Arrays.sort(citations);
-		int max = citations[citations.length-1];
-		for (int i = 0; i <= max; i++) {
+		for(int i = 1 ; i <= iT ; i++) {
+			int x1 = sc.nextInt();
+			int y1 = sc.nextInt();
+			int x2 = sc.nextInt();
+			int y2 = sc.nextInt();
 			
+			int x = Math.abs(x1 - x2);
+			int y = Math.abs(y1 - y2);
+			int cnt = 0;
+			int result = 0;
+			if(x > y) {
+				cnt = x - y;
+				if(cnt % 2 == 1) {
+					result += y * 2;
+					result += (cnt - 1) * 2 + 1;
+				}
+				else {
+					result += x * 2;
+				}
+			}
+			else {
+				cnt = y - x;
+				if(cnt % 2 == 1) {
+					result += x * 2;
+					result += (cnt - 1) * 2 + 1;
+				}
+				else {
+					result += y * 2;
+				}
+			}
+			
+			System.out.println("#"+i+" "+result);
 		}
->>>>>>> 5dffd3e9e3e0d0e91dec4bbf4c24f2358c3af02c
 	}
 }

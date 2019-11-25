c5dd9ef3466aea0f616418cf423ebe1738930e41
김경태 <kkt3526@naver.com>
2019-11-25 17:57:08 +0900

    2019-11-25

--- src/Solution_8825.java
+++ src/Solution_8825.java
@@ -2,26 +2,46 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution_8825 {
+	static int floor;
 	static int[] first_floor;
 	
 	public static void init(Scanner sc) {
-			int N = sc.nextInt();
-			first_floor = new int[N * 2 - 1];
-			
-			for (int n = 0; n < first_floor.length; n++) {
-				first_floor[n] = sc.nextInt();
-			}
-			
+		floor = sc.nextInt();
+		first_floor = new int[floor * 2 - 1];
 		
+		for (int n = 0; n < first_floor.length; n++) {
+			first_floor[n] = sc.nextInt();
+		}
 	}
 	
+	public static void binary() {
+		int left = 1;
+		int right = first_floor.length;
+		
+		while(left <= right) {
+			int middle = (left + right) / 2;
+			
+			if(solution(middle)) {
+				left = middle + 1;
+			}
+			else {
+				right = middle - 1;
+			}
+		}
+	}
+	
+	public static boolean solution(int value) {
+		//TODO 크거나 같으면 1 작으면 0, 중간의 값이 110, 011 이면 무조건 가능 101, 010 일경우 양변이 모두 0 일경우 추가 경우를 체크해야함
+		
+		return true;
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int iT = sc.nextInt();
+		int tc = sc.nextInt();
 		
-		for(int i = 1; i <= iT; i++) {
+		for(int t = 1; t <= tc; t++) {
 			init(sc);
-			System.out.println(Arrays.toString(first_floor));
+			
 		}
 
 	}
---
+++ src/Solution_8888.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+public class Solution_8888 {
+	static int[][] ranking;
+	static Point[] rank;
+	static int result, me;
+	public static void init(Scanner sc) {
+		int people = sc.nextInt();
+		int test = sc.nextInt();
+		me = sc.nextInt();
+		
+		result = 0;
+		ranking = new int[people][test];
+		
+		for (int i = 0; i < ranking.length; i++) {
+			String line = sc.nextLine();
+			String[] num = line.split(" ");
+			for (int j = 0; j < ranking[0].length; j++) {
+				ranking[i][j] = Integer.parseInt(num[j]);
+			}
+		}
+	}
+	
+	public static void knowRank() {
+		rank = new Point[ranking.length];
+		for (int i = 0; i < rank.length; i++) {
+			rank[i] = new Point(0,0);
+		}
+		
+		int count;
+		for (int p = 0; p < ranking[0].length; p++) {
+			count = 0;
+			for (int c = 0; c < rank.length; c++) {
+				if(ranking[c][p] == 0) {
+					count++;
+				}
+				else {
+					rank[c].test_count++;
+				}
+			}
+			for (int c = 0; c < rank.length; c++) {
+				if(ranking[c][p] == 1) {
+					rank[c].test_point += count;
+				}
+			} 
+		}
+		
+		
+		for (int i = 0; i < me; i++) {
+			if(rank[i].test_point > rank[me-1].test_point) {
+				result++;
+			}
+			else if(rank[i].test_point == rank[me-1].test_point) {
+				if(rank[i].test_count >= rank[me-1].test_count) {
+					result++;
+				}
+			}
+		}
+		
+		for(int i = me ; i < rank.length; i++) {
+			if(rank[i].test_point > rank[me-1].test_point) {
+				result++;
+			}
+			else if(rank[i].test_point == rank[me-1].test_point) {
+				if(rank[i].test_count > rank[me-1].test_count) {
+					result++;
+				}
+			}
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int tc = sc.nextInt();
+		
+		for(int t = 1 ; t <= tc; t++) {
+			init(sc);
+			knowRank();
+			System.out.println("#"+t+" "+rank[me-1].test_point+" "+result);
+		}
+		
+	}
+	static class Point{
+		int test_point, test_count;
+
+		public Point(int test_point, int test_count) {
+			super();
+			this.test_point = test_point;
+			this.test_count = test_count;
+		}
+
+		@Override
+		public String toString() {
+			return "Point [test_point=" + test_point + ", test_count=" + test_count + "]";
+		}
+		
+		
+	}
+}
